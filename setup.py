38db500b7764e1743ebe002239a48d768709fd88
Brian Kimathi <bryo.kim1@gmail.com>
2023-10-24 00:27:46 +0300

    Fixed package resolution

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+"""trashify setup"""
+
 from setuptools import setup, find_packages
 
 VERSION = "1.0.0"
@@ -5,14 +7,14 @@ VERSION = "1.0.0"
 
 # Setting up
 setup(
-    # the name must match the folder name 'pycheckdoc'
+    # the name must match the folder name 'trashify'
     name="trashify",
     version=VERSION,
     author="Brian Kimathi",
     author_email="<bryo.kim1@gmail.com>",
     # description=DESCRIPTION,
     # long_description=LONG_DESCRIPTION,
-    packages=find_packages("./trashify", exclude=["test*"]),
+    packages=find_packages(exclude=["test*"]),
     install_requires=[],  # add any additional packages that
     # needs to be installed along with your package. Eg: 'caer'
     keywords=["python", "documentation"],
