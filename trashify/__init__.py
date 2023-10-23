6779ef3eb83884103c521d0051e7e46f3fc61ae8
Brian Kimathi <bryo.kim1@gmail.com>
2023-10-24 01:04:23 +0300

    Add user authentication to the flask app

--- trashify/__init__.py
+++ trashify/__init__.py
@@ -1,8 +1,42 @@
+"""Flask app initialization"""
+
 from flask import Flask
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
+from os import environ as env
 
-app = Flask(__name__)
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
 
 
-@app.route("/")
-def home():
-    return "Home"
+oauth = OAuth()
+oauth.register(
+    "trashify",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    api_base_url=f'https://{env.get("AUTH0_DOMAIN")}',
+    access_token_url=f'https://{env.get("AUTH0_DOMAIN")}/oauth/token',
+    authorize_url=f'https://{env.get("AUTH0_DOMAIN")}/authorize',
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/'
+    + ".well-known/openid-configuration",
+)
+
+
+def create_app():
+    """Create a Flask app"""
+
+    app = Flask(__name__)
+
+    app.secret_key = env.get("APP_SECRET_KEY")
+    oauth.init_app(app)
+
+    from trashify.views import auth_view
+
+    app.register_blueprint(auth_view)
+
+    return app
